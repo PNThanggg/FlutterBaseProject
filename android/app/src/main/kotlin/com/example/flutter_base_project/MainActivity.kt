c6e5be882a26e8c1543afb7aa74082785b5b4466
PNT <92286791+PNThanggg@users.noreply.github.com>
2024-01-11 02:11:00 +0700

    #android: setup splash screen

--- android/app/src/main/kotlin/com/example/flutter_base_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_base_project/MainActivity.kt
@@ -1,6 +1,20 @@
 package com.example.flutter_base_project
 
+import android.os.Build
+import android.os.Bundle
+import android.window.SplashScreenView
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Aligns the Flutter view vertically with the window.
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen
+                .setOnExitAnimationListener { splashScreenView: SplashScreenView -> splashScreenView.remove() }
+        }
+
+        super.onCreate(savedInstanceState)
+    }
 }
